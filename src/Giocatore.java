f077d99d98f85aaaab3efcae58c07a7508279967
Erchask <chadi.kini07@gmail.com>
2025-10-23 12:03:02 +0200

    primo commit

--- src/Giocatore.java
+++ src/Giocatore.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 
+/**
+ * @author ChadiKini
+ */
 public class Giocatore extends Thread{
     private String nome;
     private int punteggio;
@@ -7,22 +10,33 @@ public class Giocatore extends Thread{
     GestoreGioco gg;
 
 
-    public Giocatore(String nome){
-        this.punteggio=0;
-        this.nome=nome;
-
-    }
+    /**
+     *
+     * @param nome
+     * @param gestore
+     */
     public Giocatore(String nome, GestoreGioco gestore){
         this.nome=nome;
         this.gg=gestore;
+        this.punteggio=0;
+
     }
     public void comunica(){
         System.out.println("sono il giocatore" + nome);
     }
 
+    /**
+     *
+     * @return
+     */
     public int getPunteggio() {
         return punteggio;
     }
+
+    /**
+     *
+     * @return
+     */
     public String getNome(){
         return nome;
     }
