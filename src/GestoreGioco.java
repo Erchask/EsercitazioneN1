05f6ed3f91146b7113266623944a71febd9968fb
Erchask <chadi.kini07@gmail.com>
2025-11-09 13:38:41 +0100

    Update GestoreGioco.java

--- src/GestoreGioco.java
+++ src/GestoreGioco.java
@@ -1,17 +1,43 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class GestoreGioco {
     int numeroVincente;
 
-    public GestoreGioco(int numeroVincente) {
-        this.numeroVincente=numeroVincente;
+    // Costruttore con difficoltà
+    public GestoreGioco(int livello) {
+        Random random = new Random();
+        switch(livello) {
+            case 1: 
+                numeroVincente = random.nextInt(10) + 1;
+                break;
+            case 2: 
+                numeroVincente = random.nextInt(20) + 1;
+                break;
+            case 3: 
+                numeroVincente = random.nextInt(30) + 1;
+                break;
+            default:
+                numeroVincente = random.nextInt(20) + 1; 
+        }
     }
 
+    public void verifica(int num, Giocatore giocatore) {
+        if(num == numeroVincente) {
+            System.out.println(giocatore.getNome() + " hai vinto");
+            return; 
+        } else {
+            System.out.println(giocatore.getNome() +" hai perso");
+        }
 
-    public void verifica( int num){
-    if(num==numeroVincente){
-        System.out.println("hai vinto");
-    } else{
-        System.out.println("hai perso");
-    }
-    }
+        System.out.println("per ritentare digitare si");
+        Scanner ritenta = new Scanner(System.in);
+        String risposta = ritenta.nextLine();
 
+        if(risposta.equalsIgnoreCase("si")) {
+            giocatore.gioca();
+        } else {
+            System.out.println("hai finito il gioco");
+        }
+    }
 }
